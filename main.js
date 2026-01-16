e52d36b6678b051cf4866be64ba76e225d1c712f
devsaheerhost <sheerbabu549@gmail.com>
2026-01-16 20:43:50 +0530

    still testing

--- main.js
+++ main.js
@@ -10,18 +10,21 @@ App.ui = {
 const path = window.location.pathname;
 // "/track/2423/A9X2"
 
-const parts = path.split('/').filter(Boolean);
-// ["track", "2423", "A9X2"]
+const parts = location.pathname.split('/').filter(Boolean);
+// ["s", "mobifixer-areekode", "track", "1000", "ab12"]
 
-const [, jobId, token] = parts;
-
-console.log(jobId); // 2423
-console.log(token); // A9X2
-
-alert(jobId)
-alert(token)
-
-if (parts[0] !== 'track' || !jobId || !token) {
-  document.body.innerHTML = "Invalid tracking link";
+if (parts[0] !== 's' || parts[2] !== 'track') {
+  alert('invalid')
   throw new Error("Invalid route");
 }
+
+const shopSlug = parts[1];
+const jobId = parts[3];
+const token = parts[4];
+
+alert(shopSlug)
+alert(jobId); // 2423
+alert(token); // A9X2
+
+
+
